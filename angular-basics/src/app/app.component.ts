f768fbe8194b40e36951c6e6e36873c7a7090bd3
AnnShulyndina <annshulyndina@gmail.com>
2021-01-25 19:01:06 +0300

    'loops_ngFor'

--- angular-basics/src/app/app.component.ts
+++ angular-basics/src/app/app.component.ts
@@ -6,7 +6,20 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
- toggle: any = false
+arr = [1, 1, 3, 5, 8, 12]
+
+  objs = [
+    {title: 'Post1', author: 'Vladilen', comments: [
+        {name: 'Max', text: 'lorem 1'},
+        {name: 'Max', text: 'lorem 2'},
+        {name: 'Max', text: 'lorem 3'},
+      ]},
+    {title: 'Post2', author: 'Vladilen2', comments: [
+        {name: 'Max', text: 'lorem1'},
+        {name: 'Max', text: 'lorem2'},
+        {name: 'Max', text: 'lorem3'}
+      ]}
+      ]
 }
 
 
