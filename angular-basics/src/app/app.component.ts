757c00e321bf2e528770b371940278712789ce39
AnnShulyndina <annshulyndina@gmail.com>
2021-01-25 16:58:57 +0300

    'directive-ngStyle'

--- angular-basics/src/app/app.component.ts
+++ angular-basics/src/app/app.component.ts
@@ -6,13 +6,7 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title1="IamRedLemon"
-
-  onInput(event: any) {
-    this.title1 = event.target.value
-  }
-
-  title = "initial"
+ backgroundToggle = false
 }
 
 
