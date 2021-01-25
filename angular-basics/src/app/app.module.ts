f1f822669aa088e5861e4870ce7881fabb9a8deb
AnnShulyndina <annshulyndina@gmail.com>
2021-01-25 15:45:07 +0300

    'event_bindings'

--- angular-basics/src/app/app.module.ts
+++ angular-basics/src/app/app.module.ts
@@ -6,6 +6,7 @@ import {PostComponent} from "./post/post.component";
 import { Post2Component } from './post2/post2.component';
 import { Post3Component } from './post3/post3.component';
 import {Post4Component} from "./post4/post4.component";
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import {Post4Component} from "./post4/post4.component";
     Post4Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
