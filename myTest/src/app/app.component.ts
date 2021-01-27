e4df473fe3bcada058a1fc2f79845294244bc7e4
AnnShulyndina <annshulyndina@gmail.com>
2021-01-27 11:58:16 +0300

    'first-step(app.ts)'

--- myTest/src/app/app.component.ts
+++ myTest/src/app/app.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+
+export interface Post {
+  title: string
+  text: string
+  id?: number
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +12,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'myTest';
+  listOfPosts: Post[] = [
+    {title: 'This is my first title', text: 'This text will be invisible', id: 0},
+    {title: 'No need title', text: 'No need '}
+  ]
+
 }
