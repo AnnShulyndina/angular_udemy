dc6dc5b726de5fc77b5b7cf006f09470903e45eb
AnnShulyndina <annshulyndina@gmail.com>
2021-01-29 15:46:29 +0300

    'ViewEncapsulation'

--- angular-basics/src/app/post/post.component.ts
+++ angular-basics/src/app/post/post.component.ts
@@ -14,7 +14,8 @@ import {
   OnDestroy,
   OnInit,
   Output,
-  SimpleChanges
+  SimpleChanges,
+  ViewEncapsulation
 } from '@angular/core';
 import {Post} from "../app.component";
 
@@ -22,7 +23,8 @@ import {Post} from "../app.component";
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.scss'],
-  changeDetection: ChangeDetectionStrategy.Default
+  changeDetection: ChangeDetectionStrategy.Default,
+  encapsulation: ViewEncapsulation.None
 })
 export class PostComponent implements OnInit,
   OnChanges,
