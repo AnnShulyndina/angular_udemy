444af3e5b18e4f69f0170906d3df40f1064e5f30
AnnShulyndina <annshulyndina@gmail.com>
2021-01-29 16:38:17 +0300

    'create_directive'

--- directives/src/app/app.module.ts
+++ directives/src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule} from "@angular/forms";
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import {StyleDirective} from "./directives/style.directive";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StyleDirective
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
