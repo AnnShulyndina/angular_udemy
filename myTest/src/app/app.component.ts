5984f5ca7a6ce402cff3f06d293e738389f0f246
AnnShulyndina <annshulyndina@gmail.com>
2021-01-29 12:27:57 +0300

    smth-wrong

--- myTest/src/app/app.component.ts
+++ myTest/src/app/app.component.ts
@@ -1,8 +1,8 @@
 import {Component} from '@angular/core';
 
 export interface Post {
-  title: string
-  text: string
+  title: string;
+  text: string;
   id?: number
 }
 
@@ -12,9 +12,15 @@ export interface Post {
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  listOfPosts: Post[] = [
-    {title: 'This is my first title', text: 'This text will be invisible', id: 0},
-    {title: 'No need title', text: 'No need '}
-  ]
+
+postLists: Post [] = [
+  {title: 'Fresh News', text: 'Everyone knows who did it', id: 1},
+  {title: 'You asking why', text: 'Because your game is dangerous', id: 2}
+]
+
+  updatePosts(post: Post){
+  this.postLists.unshift(post)
+  // console.log('Post', post)
+  }
 
 }
--- myTest/src/app/app.module.ts
+++ myTest/src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule} from "@angular/forms";
 
 import { AppComponent } from './app.component';
 import { PostComponent } from './post/post.component';
 import { PostFormComponent } from './post-form/post-form.component';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +14,8 @@ import { PostFormComponent } from './post-form/post-form.component';
     PostFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
