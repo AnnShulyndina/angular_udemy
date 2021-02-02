7579c9a62be668c1c9d6f02b452c819163ba4d15
AnnShulyndina <annshulyndina@gmail.com>
2021-02-02 09:42:43 +0300

    'pipe_str'

--- pipes/src/app/app.module.ts
+++ pipes/src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule} from "@angular/forms";
 
 import { AppComponent } from './app.component';
-import {FormsModule} from "@angular/forms";
+
 import {MultByPipe} from "./pipes/mult-by.pipe";
+import { ExMarksPipe } from './pipes/ex-marks.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MultByPipe
+    MultByPipe,
+    ExMarksPipe
   ],
   imports: [
     BrowserModule,
