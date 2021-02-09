2f6bcfee198162a3e6da7983741c073684d9eb2c
AnnShulyndina <annshulyndina@gmail.com>
2021-02-09 11:40:08 +0300

    'visibility_services'

--- services/src/app/services/local-counter.service.ts
+++ services/src/app/services/local-counter.service.ts
@@ -1,9 +1,12 @@
-import { Injectable } from '@angular/core';
-
-@Injectable({
-  providedIn: 'root'
-})
 export class LocalCounterService {
+ counter = 0
 
-  constructor() { }
+  increase(){
+   this.counter++
+  }
+
+  decrease(){
+   this.counter--
+  }
+
 }
