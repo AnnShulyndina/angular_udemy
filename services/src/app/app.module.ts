21244483479cdbbbdab35bfceaa1313142e6cace
AnnShulyndina <annshulyndina@gmail.com>
2021-02-09 09:48:55 +0300

    'injectable'

--- services/src/app/app.module.ts
+++ services/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import {AppCounterService} from "./services/app-counter.service";
+
 
 @NgModule({
   declarations: [
@@ -12,7 +12,7 @@ import {AppCounterService} from "./services/app-counter.service";
     BrowserModule
   ],
   providers: [
-    AppCounterService
+
   ],
   bootstrap: [AppComponent]
 })
