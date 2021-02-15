2db4de92b71a0e497b9022252549a5bed5726370
AnnShulyndina <annshulyndina@gmail.com>
2021-02-15 15:02:29 +0300

    'navigate'

--- routing/src/app/home/home.component.ts
+++ routing/src/app/home/home.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(public router: Router) {
   }
 
+  goToPostsPage() {
+    this.router.navigate(['/posts'])
+  }
 }
