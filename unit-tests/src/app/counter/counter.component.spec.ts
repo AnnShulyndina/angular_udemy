a740609410036f58ebdb1e0d51fc7af10553dd72
AnnShulyndina <annshulyndina@gmail.com>
2021-02-19 10:06:44 +0300

    'create-template-for-test'

--- unit-tests/src/app/counter/counter.component.spec.ts
+++ unit-tests/src/app/counter/counter.component.spec.ts
@@ -1,40 +1,18 @@
 import {CounterComponent} from "./counter.component";
-import {FormBuilder} from "@angular/forms";
+import {ComponentFixture, TestBed} from "@angular/core/testing";
 
 describe('CounterComponent', () => {
   let component: CounterComponent
+  let fixture: ComponentFixture<CounterComponent>
 
   beforeEach(() => {
-    component = new CounterComponent(new FormBuilder())
+    TestBed.configureTestingModule({
+      declarations: [CounterComponent]
+    })
+    fixture = TestBed.createComponent(CounterComponent)
+    component = fixture.componentInstance
   })
-//beforeAll, afterEach, afterAll
-
-  it('should increment counter by 1 ', () => {
-    component.increment()
-    expect(component.counter).toBe(1)
-  })
-  //xit - skip this test
-  it('should increment counter by 1 ', () => {
-    component.decrement()
-    expect(component.counter).toBe(-1)
-  })
-
-  it('should increment value by event emitter', () => {
-    let result = null
-    component.counterEmitter.subscribe(v => result = v)
-    component.increment()
-    expect(result).toBe(1)
-  })
-
-  it('should create form with 2 controls', ()=>{
-    expect(component.form.contains('login')).toBeTruthy()
-    expect(component.form.contains('email')).toBeTruthy()
-  })
-
-  it('should mark login as invalid if empty value', ()=>{
-    const control = component.form.get('login')
-    control.setValue('')
-    expect(control.valid).toBeFalsy()
+  it('should be created', ()=>{
+    expect(component).toBeDefined()
   })
 })
-
