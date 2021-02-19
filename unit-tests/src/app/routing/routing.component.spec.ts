744479fa666ff8be032fbebc6d64939250573d74
AnnShulyndina <annshulyndina@gmail.com>
2021-02-19 17:26:07 +0300

    'testing-router'

--- unit-tests/src/app/routing/routing.component.spec.ts
+++ unit-tests/src/app/routing/routing.component.spec.ts
@@ -1,14 +1,29 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { RoutingComponent } from './routing.component';
+import {RoutingComponent} from './routing.component';
+import {Observable} from "rxjs";
+import {ActivatedRoute, Params, Router} from "@angular/router";
+
+class RouterStub {
+  navigate(path: string[]) {
+  }
+}
+
+class ActivatedRouteStub {
+  params: Observable<Params>
+}
 
 describe('RoutingComponent', () => {
   let component: RoutingComponent;
   let fixture: ComponentFixture<RoutingComponent>;
 
-  beforeEach( () => {
+  beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [ RoutingComponent ]
+      declarations: [RoutingComponent],
+      providers: [
+        {provide: Router, useClass: RouterStub},
+        {provide: ActivatedRoute, useClass: ActivatedRouteStub},
+      ]
     })
     fixture = TestBed.createComponent(RoutingComponent);
     component = fixture.componentInstance;
@@ -17,4 +32,11 @@ describe('RoutingComponent', () => {
   it('should create', () => {
     expect(component).toBeDefined();
   });
+
+  it('should navigate to posts if go back', () => {
+    let router = TestBed.inject(Router)
+    let spy = spyOn(router, 'navigate')
+
+    expect(spy).toHaveBeenCalledWith(['/posts'])
+  })
 });
