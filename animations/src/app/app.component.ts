febad5e1037646422d6b504951c538bc4d2f05d1
AnnShulyndina <annshulyndina@gmail.com>
2021-02-24 15:11:52 +0300

    'transition'

--- animations/src/app/app.component.ts
+++ animations/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {animate, state, style, transition, trigger} from "@angular/animations";
 
 @Component({
@@ -12,15 +12,26 @@ import {animate, state, style, transition, trigger} from "@angular/animations";
         background: 'red',
         transform: 'scale(1.2)'
       })),
+      state('special', style({
+        background: 'orange',
+        transform: 'scale(0.5)',
+        borderRadius: '50%'
+      })),
       transition('start=>end', animate(450)),
-      transition('end=>start', animate('800ms ease-in-out'))
+      transition('end=>start', animate('800ms ease-in-out')),
+      transition('special<=>*', [
+        style({background: 'green'}),
+        animate(1000,
+          style({background: 'pink'})),
+        animate(750)
+      ]),
     ])
   ]
 })
 export class AppComponent {
-boxState = 'start'
+  boxState = 'start'
 
   animate() {
-    this.boxState = this.boxState === 'end' ? 'start': 'end'
+    this.boxState = this.boxState === 'end' ? 'start' : 'end'
   }
 }
